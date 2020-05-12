ed7f79d4145938eb485fb7cf2c7d13cee994e90f
deepakchaturvedifzd <61619479+deepakchaturvedifzd@users.noreply.github.com>
2020-05-12 20:27:11 +0530

    Add files via upload

---
+++ conditionalProgramming.py
@@ -0,0 +1,12 @@
+print('conditional statement')
+
+istr=input('Enter anything :')
+try:
+    astr=int(istr)
+    print("You entered number")
+    if astr<100:
+        print('Number is less than 100')
+    else:
+        print('Number is greater or equal to 100')
+except:
+    print("You entered characters")
