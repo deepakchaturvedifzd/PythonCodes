7a2df8b46a99e9debbf3241f8a0f8016efabba7a
deepakchaturvedifzd <61619479+deepakchaturvedifzd@users.noreply.github.com>
2020-05-14 21:07:56 +0530

    Add files via upload

---
+++ file_handling.py
@@ -0,0 +1,18 @@
+fhand = open("project description.txt")
+print(fhand)
+fr=fhand.read()
+print(len(fr)) #this shows number of characters in the file
+print(fr[:])
+
+# to check the no. of lines
+# or to get how many times the loop executes
+
+count=0
+for line in fhand :
+    count = count + 1
+    line=line.rstrip()
+    if not line.startswith("From"):
+        continue
+    print(line)
+
+print("Number of lines are : ",count)
